ba5f5f03db054c22944fc8096604b355c6e45bc8
maxbildner <mbildner@icloud.com>
2021-10-23 00:24:32 -0400

    update notes

--- src/components/__test__/button.test.js
+++ src/components/__test__/button.test.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Button from "../button/button";
-import { render } from "@testing-library/react";
+import { render, cleanup } from "@testing-library/react";
 // import "jest-dom/extend-expect";   // jest-dom has moved to @testing-library/jest-dom
 import "@testing-library/jest-dom";
+// import renderer from "react-test-render";
+import renderer from "react-dom/test-utils";
 
 // when you run the __test__ file,
 // - jest looks for the file names that end with .test.js
 // - to run tests, in terminal run "npm test"
 //   - only have to run that command once
 //   - after that, changes you make in code, will automatically be tested
+
+// after each cb in the it blocks, run the cleanup function
+// - clean up after each test so we don't have duplicate buttons
+afterEach(cleanup);
 
 // Call back function runs ?
 it("renders without crashing", () => {
@@ -25,4 +31,11 @@ it("renders button correctly", () => {
 
   // grab button based on data-testid "button", then make sure it's label value is = "click me please"
   expect(getByTestId("button")).toHaveTextContent("click me please");
+});
+
+it("matches snapshot", () => {
+  // note below doesn't work- not importing create method properly
+  const tree = renderer.create(<Button label="save"></Button>).toJSON();
+
+  expect(tree).toMatchSnapshot();
 });
